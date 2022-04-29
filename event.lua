ee19f337019c36ae009783b4be74fa6b8ead86ed
TommyRBLX <46384146+TommyRBLX@users.noreply.github.com>
2022-04-29 12:24:47 +0200

    Update 4

--- event.lua
+++ event.lua
@@ -4,22 +4,40 @@ events.__index = events
 local connections = {}
 connections.__index = {}
 
-local random = math.random
-local function uuid()
-    local uuid ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-
-    return string.gsub(uuid, '[xy]', function (c)
-        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
-        return string.format('%x', v)
-    end)
-end
+function table_print (tt, indent, done)
+    done = done or {}
+    indent = indent or 0
+    if type(tt) == "table" then
+      for key, value in pairs (tt) do
+        io.write(string.rep (" ", indent)) -- indent it
+        if type (value) == "table" and not done [value] then
+          done [value] = true
+          io.write(string.format("[%s] => table\n", tostring (key)));
+          io.write(string.rep (" ", indent+4)) -- indent it
+          io.write("(\n");
+          table_print (value, indent + 7, done)
+          io.write(string.rep (" ", indent+4)) -- indent it
+          io.write(")\n");
+        else
+          io.write(string.format("[%s] => %s\n",
+              tostring (key), tostring(value)))
+        end
+      end
+    else
+      io.write(tt .. "\n")
+    end
+  end
 
 function connections.new(id)
     local self = {}
 
-    self.id = id or uuid()
+    self.id = id
     self.f = nil
     self.fireOnDisconnect = false
+
+    function self:fire()
+        coroutine.wrap(self.f)
+    end
 
     return setmetatable(self, connections)
 end
@@ -38,7 +56,7 @@ return function(event_name)
     function self:getConnections()
         return self.connections
     end
-
+    
     function self:connect(f, id, priority)
         local connection = connections.new(id)
         id = id or tostring(#self.connections+1)
@@ -56,25 +74,33 @@ return function(event_name)
     end
 
     function self:disconnect(id)
-        for _,connection in pairs(self.connections) do
-            connection
+        if not id then
+            for _,connection in pairs(self.connections) do connection:disconnect() end
+        else
+            self.connections[id]:disconnect()
         end
     end
 
     function self:fire(id)
         if not id then
-            for _,f in pairs(self.connections) do f.connection()end
+            for _,connection in pairs(self.connections) do connection:fire() end
         else
-            self.connections[id].f()
+            self.connections[id]:fire()
         end
     end
-    
-    function self:remove()
-        events[#events]
-    end
 
-    self = setmetatable(self, events)
-    events[#events+1] = self
+
+
+    self = setmetatable(self, {
+        __index = function()
+            
+        end,
+
+        __tostring = function()
+            
+        end
+    })
+    events[self.name] = self
 
     return self
 end
